a2e194512f1752ca13ee38d80be014e39f4d1726
Andy Dietz <andy.dietz@regionalcare.com>
2016-07-27 16:07:33 -0600

    Adds support for CTRL-A (select all) Auto-formats on CTRL-V Allows Font Resizing

--- TransformTester/Form1.Designer.cs
+++ TransformTester/Form1.Designer.cs
@@ -31,21 +31,29 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.exitToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.formXMLSourceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.formatTransformToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripComboBox1 = new System.Windows.Forms.ToolStripComboBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.formXMLSourceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.formatTransformToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.menuStrip1.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip1
             // 
+            this.menuStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Visible;
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.exitToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(10, 10);
@@ -59,7 +67,8 @@
             this.exitToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.exitToolStripMenuItem1,
             this.formXMLSourceToolStripMenuItem,
-            this.formatTransformToolStripMenuItem});
+            this.formatTransformToolStripMenuItem,
+            this.toolStripComboBox1});
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
             this.exitToolStripMenuItem.Text = "File";
@@ -67,14 +76,33 @@
             // exitToolStripMenuItem1
             // 
             this.exitToolStripMenuItem1.Name = "exitToolStripMenuItem1";
-            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(169, 22);
+            this.exitToolStripMenuItem1.Size = new System.Drawing.Size(181, 22);
             this.exitToolStripMenuItem1.Text = "Exit";
             this.exitToolStripMenuItem1.Click += new System.EventHandler(this.exitToolStripMenuItem1_Click);
+            // 
+            // formXMLSourceToolStripMenuItem
+            // 
+            this.formXMLSourceToolStripMenuItem.Name = "formXMLSourceToolStripMenuItem";
+            this.formXMLSourceToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.formXMLSourceToolStripMenuItem.Text = "Format XML Source";
+            this.formXMLSourceToolStripMenuItem.Click += new System.EventHandler(this.formXMLSourceToolStripMenuItem_Click);
+            // 
+            // formatTransformToolStripMenuItem
+            // 
+            this.formatTransformToolStripMenuItem.Name = "formatTransformToolStripMenuItem";
+            this.formatTransformToolStripMenuItem.Size = new System.Drawing.Size(181, 22);
+            this.formatTransformToolStripMenuItem.Text = "Format Transform";
+            this.formatTransformToolStripMenuItem.Click += new System.EventHandler(this.formatTransformToolStripMenuItem_Click);
+            // 
+            // toolStripComboBox1
+            // 
+            this.toolStripComboBox1.Name = "toolStripComboBox1";
+            this.toolStripComboBox1.Size = new System.Drawing.Size(121, 23);
             // 
             // textBox2
             // 
             this.textBox2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox2.Location = new System.Drawing.Point(777, 28);
+            this.textBox2.Location = new System.Drawing.Point(777, 63);
             this.textBox2.MaxLength = 3276700;
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
@@ -82,11 +110,12 @@
             this.textBox2.Size = new System.Drawing.Size(768, 1279);
             this.textBox2.TabIndex = 2;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textbox2_keydown);
             // 
             // textBox3
             // 
             this.textBox3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox3.Location = new System.Drawing.Point(1551, 28);
+            this.textBox3.Location = new System.Drawing.Point(1551, 63);
             this.textBox3.MaxLength = 3276700;
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
@@ -103,7 +132,7 @@
             this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label1.Location = new System.Drawing.Point(3, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(768, 25);
+            this.label1.Size = new System.Drawing.Size(768, 30);
             this.label1.TabIndex = 4;
             this.label1.Text = "XML Source";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -114,7 +143,7 @@
             this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label2.Location = new System.Drawing.Point(777, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(768, 25);
+            this.label2.Size = new System.Drawing.Size(768, 30);
             this.label2.TabIndex = 5;
             this.label2.Text = "Transform Text";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -125,74 +154,115 @@
             this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.label3.Location = new System.Drawing.Point(1551, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(770, 25);
+            this.label3.Size = new System.Drawing.Size(770, 30);
             this.label3.TabIndex = 6;
             this.label3.Text = "Transform Output";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // tableLayoutPanel1
             // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.tableLayoutPanel1.ColumnCount = 3;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33332F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
-            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox1, 0, 2);
             this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.textBox3, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox3, 2, 2);
             this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.label3, 2, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(10, 34);
+            this.tableLayoutPanel1.Controls.Add(this.button2, 1, 1);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(10, 80);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 25F));
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(2324, 1310);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(2324, 1264);
             this.tableLayoutPanel1.TabIndex = 8;
             this.tableLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.tableLayoutPanel1_Paint);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 19);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Format Input";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.formatButton1_Click);
+            // 
             // textBox1
             // 
+            this.textBox1.AcceptsReturn = true;
+            this.textBox1.AcceptsTab = true;
             this.textBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.textBox1.Location = new System.Drawing.Point(3, 28);
+            this.textBox1.Location = new System.Drawing.Point(3, 63);
             this.textBox1.MaxLength = 3276700;
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.textBox1.Size = new System.Drawing.Size(768, 1279);
             this.textBox1.TabIndex = 7;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textbox1_keydown);
             // 
-            // formXMLSourceToolStripMenuItem
+            // button2
             // 
-            this.formXMLSourceToolStripMenuItem.Name = "formXMLSourceToolStripMenuItem";
-            this.formXMLSourceToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
-            this.formXMLSourceToolStripMenuItem.Text = "Format XML Source";
-            this.formXMLSourceToolStripMenuItem.Click += new System.EventHandler(this.formXMLSourceToolStripMenuItem_Click);
+            this.button2.Location = new System.Drawing.Point(777, 33);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(123, 23);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Format XSLT";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.formatButton2_Click);
             // 
-            // formatTransformToolStripMenuItem
+            // pictureBox1
             // 
-            this.formatTransformToolStripMenuItem.Name = "formatTransformToolStripMenuItem";
-            this.formatTransformToolStripMenuItem.Size = new System.Drawing.Size(178, 22);
-            this.formatTransformToolStripMenuItem.Text = "Format Transform";
-            this.formatTransformToolStripMenuItem.Click += new System.EventHandler(this.formatTransformToolStripMenuItem_Click);
+            this.pictureBox1.Image = global::TransformTester.Properties.Resources.format_font_size_more;
+            this.pictureBox1.Location = new System.Drawing.Point(2232, 37);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TransformTester.Properties.Resources.format_font_size_less;
+            this.pictureBox2.Location = new System.Drawing.Point(2283, 37);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(2344, 1354);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Padding = new System.Windows.Forms.Padding(10);
             this.Text = "XSLT Tester";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -212,6 +282,11 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.ToolStripMenuItem formXMLSourceToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem formatTransformToolStripMenuItem;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- TransformTester/Form1.cs
+++ TransformTester/Form1.cs
@@ -12,8 +12,13 @@ using System.Xml.Xsl;
 using System.Xml.Linq;
 namespace TransformTester
 {
+
+
     public partial class Form1 : Form
     {
+
+        private int fontSize = 12;
+
         public Form1()
         {
             InitializeComponent();
@@ -105,10 +110,99 @@ namespace TransformTester
             textBox2.Text = FormatXml(textBox2.Text);
         }
 
-        private void textBox1_TextChanged_1(object sender, EventArgs e)
+
+        private void formatButton1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = FormatXml(textBox1.Text);
+        }
+
+
+        private void formatButton2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = FormatXml(textBox2.Text);
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
         {
 
-            transform();
+        }
+
+        private void textbox2_keydown(object sender, KeyEventArgs e)
+        {
+            if (e.Control & e.KeyCode == Keys.A)
+            {
+                textBox2.SelectAll();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                return;
+            }
+
+            if (e.Control & e.KeyCode == Keys.D)
+            {
+                textBox2.Text = FormatXml(textBox2.Text);
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                return;
+            }
+
+            if (e.Control & e.KeyCode == Keys.V)
+            {
+                textBox2.Text = FormatXml(Clipboard.GetText());
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                return;
+            }
+
+        }
+
+        private void textbox1_keydown(object sender, KeyEventArgs e)
+        {
+            if (e.Control & e.KeyCode == Keys.A)
+            {
+                textBox1.SelectAll();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                return;
+            }
+
+            if (e.Control & e.KeyCode == Keys.D)
+            {
+                textBox1.Text = FormatXml(textBox1.Text);
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                return;
+            }
+
+            if (e.Control & e.KeyCode == Keys.V)
+            {
+                textBox1.Text = FormatXml(Clipboard.GetText());
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+                return;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            var newFont = new Font(textBox1.Font.FontFamily, fontSize + 1);
+            textBox1.Font = newFont;
+            textBox2.Font = newFont;
+            textBox3.Font = newFont;
+            fontSize++;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            var newFont = new Font(textBox1.Font.FontFamily, fontSize - 1);
+            textBox1.Font = newFont;
+            textBox2.Font = newFont;
+            textBox3.Font = newFont;
+            fontSize--;
         }
     }
 }
